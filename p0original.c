75d95f3c09d9e173578eef5a37aeba56c264d619
Supriyayerva <93111012+Supriyayerva@users.noreply.github.com>
2021-12-27 06:09:34 +0000

    p0debug

--- p0original.c
+++ p0original.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main()
 {
-char name[500];
-printf("enter your name\n");
-scanf("%s",name);
-printf("%s",name);
-return 0;
+  char name[500];
+  printf("enter your name\n");
+  scanf("%s",name);
+  printf("%s",name);
+  return 0;
 }
