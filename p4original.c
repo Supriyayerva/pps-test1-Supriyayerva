0a45fa1b818b0da4067106feda50b9fdc7ee6c51
Supriyayerva <93111012+Supriyayerva@users.noreply.github.com>
2021-12-31 09:03:05 +0000

    p5debug

---
+++ p4original.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int input()
+{
+  int a;
+  printf("Enter the number:/n");
+  scanf("%d", &a);
+  return a;
+}
+int cmp(int a,int b,int c)
+{
+  if(a>b&&a>c)
+  {
+    return a;
+  }
+  else if (b>c&&b>a);
+  {
+    return b;
+  }
+  else if(c>a&&c>b)
+  {
+    return c;
+  }
+}
+void input(int a,int b,int c,int large)
+{
+  printf("the largest of %d,%d,%d is %d\n",a,b,c,large);
+
+}
+int main()
+{
+  int a,b,c,large;
+  a = input();
+  b = input();
+  c = input();
+  large=cmp(a,b,c);
+  output(a,b,c,large);
+  return 0;
+}
